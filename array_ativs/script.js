1dc8805e8e5236e1df933ea86d3788a8e2105376
William-Bos <boswilliam56@gmail.com>
2025-09-29 21:15:45 -0300

    atividades sobre arrays

---
+++ array_ativs/script.js
@@ -0,0 +1,23 @@
+const matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+
+
+
+
+let resultado = 0
+for(i=0; i < matriz.length ; i++){
+
+    for( j=0; j< matriz[i].length; j++){
+        elemento = matriz[i][j]
+        resultado = resultado + matriz[i][j]
+        
+    }
+  
+}
+console.log("A soma de todos os valores é:", resultado);
+
+
