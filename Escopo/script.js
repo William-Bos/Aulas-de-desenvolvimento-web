da275a0f00caab8cd6c81dda6390e2b97b4bea45
William-Bos <boswilliam56@gmail.com>
2025-10-13 14:49:13 -0300

    Escopo

---
+++ Escopo/script.js
@@ -0,0 +1,18 @@
+let pokemon = "Charmander"
+
+function evoluir(){
+    pokemon = "Charmeleon"
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal(){
+    let animal = "gato"
+    return animal
+}
+criarAnimal()
+
+
+console.log(animal)
